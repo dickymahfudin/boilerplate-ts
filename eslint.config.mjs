367aa230add7b224990551ad17b2e098fe122343
Dicky Mahfudin <dickymahfudin@gmail.com>
2025-01-27 02:28:37 +0700

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 // Translate ESLintRC-style configs into flat configs.
 const filename = fileURLToPath(import.meta.url);
@@ -10,48 +10,48 @@ const _dirname = dirname(filename);
 
 const compat = new FlatCompat({
   baseDirectory: _dirname,
-  recommendedConfig: pluginJs.configs.recommended,
+  recommendedConfig: pluginJs.configs.recommended
 });
 
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   ...compat.config({
     env: { node: true },
-    ignorePatterns: ['dist/**/*', 'eslint.config.mjs'],
+    ignorePatterns: ["dist/**/*", "eslint.config.mjs"],
     extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-      'plugin:import/typescript',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended",
+      "plugin:import/errors",
+      "plugin:import/warnings",
+      "plugin:import/typescript",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended"
     ],
-    parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
     parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      project: './tsconfig.json',
+      ecmaVersion: "latest",
+      sourceType: "module",
+      project: "./tsconfig.json"
     },
-    plugins: ['@typescript-eslint', 'import', 'prettier'],
+    plugins: ["@typescript-eslint", "import", "prettier"],
     rules: {
-      '@typescript-eslint/no-unused-vars': 'error',
-      '@typescript-eslint/no-empty-interface': 'error',
-      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-      'no-underscore-dangle': 'off',
-      'no-console': 'off',
-      'import/extensions': 'off',
-      'import/prefer-default-export': 'off',
-      'prettier/prettier': 'error',
-      'no-useless-constructor': 'off',
-      'no-empty-function': ['error', { allow: ['constructors'] }],
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-empty-interface": "error",
+      "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+      "no-underscore-dangle": "off",
+      "no-console": "off",
+      "import/extensions": "off",
+      "import/prefer-default-export": "off",
+      "prettier/prettier": ["error"],
+      "no-useless-constructor": "off",
+      "no-empty-function": ["error", { allow: ["constructors"] }]
     },
     settings: {
-      'import/resolver': {
+      "import/resolver": {
         typescript: {
-          project: './tsconfig.json',
-        },
-      },
-    },
-  }),
+          project: "./tsconfig.json"
+        }
+      }
+    }
+  })
 ];
